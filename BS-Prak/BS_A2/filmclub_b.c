1db3dbd11d6d11906107f232eb2f37c3a64d8af4
s0ftcorn <cornolinius@gmail.com>
2013-05-24 14:49:15 +0200

    Aufgabe b hinzugefügt 	geändert:   filmclub_b.c

--- BS-Prak/BS_A2/filmclub_b.c
+++ BS-Prak/BS_A2/filmclub_b.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
+#include <pthread.h>
 
 int kontostand;
 
-void filmebestellen(void);
-void getraenkeundspeisen(void);
-void ueberweisen(void);
+void* filmebestellen(void *pv);
+void* getraenkeundspeisen(void *pv);
+void* ueberweisen(void *pv);
 
 int main(int argc, char **argv){
 	kontostand = 1000;
-	filmebestellen();
-	ueberweisen();
-	getraenkeundspeisen();
+	
+	int status_film;
+	pthread_t thread_film;
+	int status_nahrung;
+	pthread_t thread_nahrung;
+	int status_uberweiung;
+	pthread_t thread_uberweisung;
+	
+	status_film = pthread_create(&thread_film, NULL, &filmebestellen, NULL);
+	if(status_film) printf("Fehler beim Filmthread");
+	
+	status_nahrung = pthread_create(&thread_nahrung, NULL, &getraenkeundspeisen, NULL);
+	if(status_nahrung) printf("Fehler beim Getraenke- und Speisenthread");
+	
+	status_uberweiung = pthread_create(&thread_uberweisung, NULL, &ueberweisen, NULL);
+	if(status_uberweiung) printf("Fehler beim Uberweisungsthread");
+	
+	status_film = pthread_join(thread_film, NULL);
+	if(status_uberweiung) printf("Fehler beim aufsammeln des Filmthreads");
+	
+	status_nahrung = pthread_join(thread_nahrung, NULL);
+	if(status_nahrung) printf("Fehler beim aufsammeln des Nahrungsthreads");
+	
+	status_uberweiung = pthread_join(thread_uberweisung, NULL);
+	if(status_uberweiung) printf("Fehler beim aufsammeln des Uberweisungsthread");
 	
 	printf("Endkontostand: %d\n",kontostand);
 	
-	return 0;
+	pthread_exit(NULL);
 }
 
-void filmebestellen(void){
+void* filmebestellen(void *pv){
 	int localkonto = kontostand;
 	for(int i = 0; i < 5; i++){
 		printf("Bestelle Filme\n");
@@ -26,9 +49,11 @@ void filmebestellen(void){
 		kontostand = localkonto;
 		printf("Kontostand nachher: %d\n",localkonto);
 	}
+	
+	pthread_exit(NULL);
 }
 
-void getraenkeundspeisen(void){
+void* getraenkeundspeisen(void *pv){
 	int localkonto = kontostand;
 	for(int i = 0; i < 5; i++){
 		printf("Kaufe Getraenke und Speisen\n");
@@ -37,9 +62,11 @@ void getraenkeundspeisen(void){
 		kontostand = localkonto;
 		printf("Kontostand nachher: %d\n",localkonto);
 	}
+	
+	pthread_exit(NULL);
 }
 
-void ueberweisen(void){
+void* ueberweisen(void *pv){
 	int localkonto = kontostand;
 	for(int i = 0; i < 2; i++){
 		printf("Ueberweise Geld\n");
@@ -48,4 +75,6 @@ void ueberweisen(void){
 		kontostand = localkonto;
 		printf("Kontostand nachher %d\n",localkonto);
 	}
+	
+	pthread_exit(NULL);
 }
